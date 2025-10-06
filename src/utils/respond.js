1b9d30ec5617c685b3d0ecee6bed2bc5bf0e70be
bvd63 <blagavlad63@gmail.com>
2025-10-07 02:22:26 +0300

    Update respond.js

--- src/utils/respond.js
+++ src/utils/respond.js
@@ -1,7 +1,11 @@
 export function ok(data = {}) {
-  return { ok: true, ...data };
+  // backward compatible shape: { ok: true, ...data }
+  return Object.assign({ ok: true }, data);
 }
 
-export function error(message, code = 'error', details = {}) {
-  return { error: message, code, ...details };
+export function error(message = 'error', code = 'error', details = {}) {
+  return { ok: false, error: { message, code, details } };
 }
+
+export function sendOk(reply, data = {}) { reply.code(200).send(ok(data)); }
+export function sendError(reply, message = 'error', code = 'error', status = 400, details = {}) { reply.code(status).send(error(message, code, details)); }
